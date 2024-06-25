3b2f4df18f90999c5af307c0f230508df69d6302
StasX <s.mestechkin@gmail.com>
2024-06-26 02:11:49 +0300

    Refactor main.js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,25 +1,27 @@
-
-function reverse(url) {
+function showResult(txt) {
     const result = $("#result");
-    $.post("/geturl", { "url": url }).done((data) => {
-        result.val(data.url);
-        result.css({ "display": "block" });
-    }).fail((err) => {
-        result.val("Something went wrong.");
-        result.css({ "display": "block" });
-    });
+    result.val(txt);
+    result.css({ "display": "block" });
+}
+
+function reverse() {
+    const url = $("#url").val();
+    if (/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(url)) {
+        getUrl(url);
+    } else {
+        showResult("Invalid url.");
+    }
+}
+
+function getUrl(url) {
+    $.post("/geturl", { "url": url })
+        .done((data) => showResult(data.url))
+        .fail((err) => showResult("Something went wrong."));
 }
 
 $(document).ready(() => {
-    $("#reverse").click(() => {
-        const url = $("#url").val();
-        if (/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(url)) {
-            reverse(url);
-        } else {
-            const result = $("#result");
-            result.val("Invalid url.");
-            result.css({ "display": "block" });
-        }
-
+    $("#reverse").click(reverse);
+    $("#url").on('keypress', function (e) {
+        if (e.which == 13) reverse();
     });
 });
