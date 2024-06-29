e56ea88c727aef17ce3bce0bf02ff247e6529239
StasX <s.mestechkin@gmail.com>
2024-06-29 20:54:52 +0300

    Add dashboard

--- src/views/dashboard_view.py
+++ src/views/dashboard_view.py
@@ -3,6 +3,7 @@ import requests
 from models.url_model import UrlModel
 from models.user_model import UserModel
 from mongoengine import DoesNotExist
+from random import randint
 
 
 dashboard_blueprint = Blueprint("dashboard_view", __name__)
@@ -10,4 +11,9 @@ dashboard_blueprint = Blueprint("dashboard_view", __name__)
 
 @dashboard_blueprint.route("/dashboard")
 def render():
-    return render_template("dashboard.html")
+    user = session["current_user"]
+    urls_models=UrlModel.objects(user=user["id"])
+    colors=[]
+    for i in range(len(urls_models)):
+        colors.append([randint(0,255),randint(0,255),randint(0,255)])
+    return render_template("dashboard.html",urls_data=urls_models,colors=colors)
